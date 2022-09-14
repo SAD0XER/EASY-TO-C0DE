f3a17bd4a4eb1ec7ccc91887648e03dfe8e37ef2
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-09-14 17:57:44 +0530

    Broiler Plate code of the java with I/O.

---
+++ Broiler Plate Code Of Java.java
@@ -0,0 +1,8 @@
+// import java.util.Scanner; 
+
+public clss Main {
+  public static void main(String args[]) {
+//     Scanner scan = new Scanner(System.in);
+    System.out.println("This is my Test program.");
+  }
+}
