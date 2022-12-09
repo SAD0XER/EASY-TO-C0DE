4a6d45aca6f52303f03c67c7cedfafa793634f3f
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2022-12-09 20:18:57 +0530

    Added all VS Java Codes.

---
+++ VS_JavaCodes/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner; //impoting scanner class.
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Practice of Taking input from user in all format.
+        
+        // int a = sc.nextInt();
+        // byte b = sc.nextByte();
+        // float c = sc.nextFloat();
+        // double d = sc.nextDouble();
+        // boolean e = sc.nextBoolean();
+        // long f = sc.nextLong();
+        System.out.print("Enter the string: ");
+        char ch = sc.nextChar();
+
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
+        // System.out.println(d);
+        // System.out.println(e);
+        // System.out.println(f);
+        System.out.println(ch);
+        
+
+    }
+}
---
+++ VS_JavaCodes/PrintPattern.java
@@ -0,0 +1,6 @@
+public class PrintPattern {
+    public static void main(String[] sadoxer) {
+        int N = 5;
+
+    }
+}
