2b4ce00d85b74db23603300b57704957003b581c
SAD0XER <79737447+SAD0XER@users.noreply.github.com>
2023-03-03 23:20:42 +0530

    Update and rename Broiler Plate Code.java to Boilerplate Code.java

--- Boilerplate Code.java
+++ Boilerplate Code.java
@@ -1,8 +1,7 @@
 // import java.util.Scanner; 
 
 public clss Main {
-  public static void main(String args[]) {
+  public static void main(String[] args) {
 //     Scanner scan = new Scanner(System.in);
-    System.out.println("This is my Test program.");
   }
 }
